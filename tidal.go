6ba1784193af61d37028d039196f8dddd7cb9a55
Craig Swank <craigswank@gmail.com>
2018-03-03 09:14:45 -0700

    added user playlists

--- tidal.go
+++ tidal.go
@@ -42,7 +42,6 @@ func (t *Tidal) CheckSession() (bool, error) {
 	//return False
 	var out interface{}
 	err := t.get(fmt.Sprintf("users/%s/subscription", t.UserID), nil, &out)
-	fmt.Println(out)
 	return true, err
 }
 
@@ -66,11 +65,11 @@ func (t *Tidal) GetAlbumTracks(id string) ([]Track, error) {
 }
 
 // GetPlaylistTracks func
-func (t *Tidal) GetPlaylistTracks(id string) ([]Track, error) {
+func (t *Tidal) GetPlaylistTracks(id string, l string) ([]Track, error) {
 	var s struct {
 		Items []Track `json:"items"`
 	}
-	return s.Items, t.get("playlists/"+id+"/tracks", &url.Values{}, &s)
+	return s.Items, t.get("playlists/"+id+"/tracks", &url.Values{"limit": {l}}, &s)
 }
 
 // SearchTracks func
@@ -109,6 +108,11 @@ func (t *Tidal) GetArtistAlbums(artist, l string) ([]Album, error) {
 	return s.Items, t.get(fmt.Sprintf("artists/%s/albums", artist), &url.Values{
 		"limit": {l},
 	}, &s)
+}
+
+func (t *Tidal) GetUserPlaylists() ([]Album, error) {
+	var s Search
+	return s.Items, t.get(fmt.Sprintf("users/%s/playlists", t.UserID), &url.Values{}, &s)
 }
 
 // helper function to generate a uuid
